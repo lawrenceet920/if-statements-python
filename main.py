f8bc84232d2812f269c26abcfe03ed87ae50a246
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-09-30 13:50:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# Ethan Lawrence
+# September 30 2024
+# If Statements
+# Task 1
+superheros = ['Superman', 'Batman', ' Ironman', 'Hulk', 'Spiderman']
+contain = 'Superman' in superheros
+if contain == True:
+    print('Superman is my favorite hero!')
+
+# Task 2
+score = 90
+if score >= 85:
+    print(f'You scored a B or higher!')
+
+# Task 3
+quote = 'Momma Mia Pizza Pie!'
+if quote.startswith('M'):
+    print('The quote starts with an Uppercase M')
+
+# Task 4 
+filename = 'keyboard.png'
+if filename.endswith('.png'):
+    print('Filename ends with .png!')
+
+# Task 5
+is_registered = False
+if is_registered == False:
+    print('You are not registered for classes at NMC')
+else:
+    print('You are Registered for classes at NMC')
